3aac719c5f1dc2e8c53274c545f903ff3b2facdc
redsphinx <redsphinx@users.noreply.github.com>
2016-11-25 12:15:03 +0100

    Update install_everything.sh
    added spotify

--- install_everything.sh
+++ install_everything.sh
@@ -62,3 +62,9 @@ sudo apt-get install okular
 
 echo "9. installing virtualenv"
 sudo apt-get install virtualenv
+
+echo "10. installing spotify"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
